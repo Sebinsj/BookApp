089e8a5362ae27a4c91475e41dd1645797aa31b6
Sebinsj <73657710+Sebinsj@users.noreply.github.com>
2023-11-02 13:40:03 +0530

    Add files via upload

---
+++ Project/myproject/webpages/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Book
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+
+    books=Book.objects.all()
+    if request.method=='POST':
+        if "add" in request.POST:
+            id=request.POST.get('id')
+            name=request.POST.get('name')
+            author=request.POST.get('author')
+            Book.objects.create(id=id,name=name,
+                                author=author
+
+            )
+            messages.success(request,'book added')
+
+            
+    context={
+        "books":books
+    }
+    return render(request,'webpages/home.html')
